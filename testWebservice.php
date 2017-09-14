0888a5d2049927f0778406434e0ab6ba4144361b
hansolasus <31943493+hansolasus@users.noreply.github.com>
2017-09-14 10:54:41 +0700

    test

--- testWebservice.php
+++ testWebservice.php
@@ -1,4 +1,7 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 	$client = new SoapClient("http://www.scadachaopraya.com/CPYProvider.asmx?wsdl",
 			array(
 			  "trace"      => 1,		// enable trace to view what is happening
